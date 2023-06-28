559b8d74039b3ea706459bfc79d4d17797341aa1
Steven Edwards <winehacker@gmail.com>
2023-06-28 00:26:30 -0700

    Encrypt bucket with correct key stored in KMS

--- infrastructure/bucket.tf
+++ infrastructure/bucket.tf
@@ -1,9 +1,35 @@
 module "s3" {
-    source = "./site"
+  source      = "./site"
+}
+
+locals {
+  static_application = "${path.module}/site/index.html"
+  #keyfile = "${path.module}/site/key.txt"
+}
+
+resource "aws_s3_bucket_object" "static-application" {
+  bucket      = "${var.bucket_name}"
+  source      = local.static_application
+  key		= "${aws_kms_key.s3_bucket_key.arn}"
+  #key         = local.keyfile
 }
 
 resource "aws_s3_bucket" "steven-challenge" {
-    bucket = "${var.bucket_name}" 
-    acl = "${var.acl_value}"   
+  bucket = "${var.bucket_name}"
+  acl = "${var.acl_value}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.s3_bucket_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_kms_key" "s3_bucket_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
 }
 
